efd2253aca8d7e382cbf2452b61eae4bf2ca13bc
perfomans1999 <artyrito333@bk.ru>
2021-04-04 06:13:11 +0300

    Добавьте файлы проекта.

---
+++ Algorithm/BubbleSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class BubbleSort<T> : AlgorithmBase<T> where T: IComparable
+    {
+        public override void Sort()
+        {
+            var count = Items.Count;
+            for (int j = 0; j < count; j++)
+            {
+                for (int i = 0; i < count - j - 1; i++)
+                {
+                    var a = Items[i];
+                    var b = Items[i + 1];
+
+                    if (a.CompareTo(b) == 1)
+                    {
+                        Swop(i, i + 1);
+                    }
+                }
+            }
+
+            
+        }
+    }
+}
